f4c87d1ae4e7beaeeb76d81ae69a0e4ff4fa5a3f
mahidhar13 <mahidhar.134@gmail.com>
2021-09-06 15:14:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-odjfik-vh28vy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-odjfik-vh28vy)
---
+++ src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject, combineLatest, Observable, of } from 'rxjs';
+import { filter, map, skip, startWith, take } from 'rxjs/operators';
+import { increment, decrement, reset } from '../counter.actions';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css']
+})
+export class MyCounterComponent {
+  count$: Observable<any>;
+  operations$ = new BehaviorSubject<string>('');
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+
+    // this.count$
+    //   .pipe(map(item => item * item))
+    //   .subscribe(data => console.log('Square numbers', data));
+
+    // this.count$
+    //   .pipe(take(3))
+    //   .subscribe(data => console.log('Taking only 3 numbers', data));
+
+    // this.count$
+    //   .pipe(filter(item => !(item % 2)))
+    //   .subscribe(data => console.log('Even numbers', data));
+
+    // this.count$
+    //   .pipe(skip(3))
+    //   .subscribe(data => console.log('First 3 values are skipped', data));
+
+    // this.count$
+    //   .pipe(startWith(100))
+    //   .subscribe(data => console.log('starting with 100', data));
+  }
+
+  calculate(value: string) {
+    console.log('in calculate method', value);
+    this.operations$.next(value);
+    combineLatest(this.count$, this.operations$).subscribe(
+      ([count, operation]) => {
+        console.log(operation);
+        if (value === 'addition') {
+          console.log('result of addition', count + count);
+        }
+        if (value === 'multiplication') {
+          console.log('result of multiplication', count * count);
+        }
+      }
+    );
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
